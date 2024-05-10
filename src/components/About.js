90efb1e73ac79a24b9263fc5d182b019ea702500
Vasilii Tsyganenko <vtsyganenko@unitechus.com>
2024-05-10 15:35:45 -0400

    Final change

--- src/components/About.js
+++ src/components/About.js
@@ -23,7 +23,7 @@ const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#" className="btn">
+          <a href="#top" className="btn">
             read more
           </a>
         </article>
--- src/components/SocialLink.js
+++ src/components/SocialLink.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function SocialLink({ link }) {
   return (
     <li key={link.id}>
-      <a href={link.href} target="_blank" className="nav-icon">
+      <a href={link.href} target="_blank" className="nav-icon" rel="noreferrer">
         <i className={link.icon}></i>
       </a>
     </li>
